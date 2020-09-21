36be30cf5949b440bfdbd02e70f72692d4a664e5
diogoku <diogomnribeiro@gmail.com>
2020-09-21 18:54:37 +0100

    Updates

---
+++ precache-manifest.e11ca99fecfee2816145a08abb49a507.js
@@ -0,0 +1,82 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e11ad6f8c63ddae29d0737a298858c90",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "30d0b3a43e41334a9123",
+    "url": "/portfolio/static/css/2.4707e12a.chunk.css"
+  },
+  {
+    "revision": "b7a42568a05c8a94f0fc",
+    "url": "/portfolio/static/css/main.63951676.chunk.css"
+  },
+  {
+    "revision": "30d0b3a43e41334a9123",
+    "url": "/portfolio/static/js/2.b4e30caf.chunk.js"
+  },
+  {
+    "revision": "1937a0fc6634269e5f47f8181e27fdc7",
+    "url": "/portfolio/static/js/2.b4e30caf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b7a42568a05c8a94f0fc",
+    "url": "/portfolio/static/js/main.4c8da08f.chunk.js"
+  },
+  {
+    "revision": "7a6cc99bd8f08f9fa6ec",
+    "url": "/portfolio/static/js/runtime-main.209c2d7e.js"
+  },
+  {
+    "revision": "e1dbd44cb6d69d235f454f9ce3ac642f",
+    "url": "/portfolio/static/media/backgroundAboutMe.e1dbd44c.jpg"
+  },
+  {
+    "revision": "448ccda7e4e25ed738f8ea6f0a253cfb",
+    "url": "/portfolio/static/media/backgroundMain.448ccda7.jpg"
+  },
+  {
+    "revision": "c4a06941d8c3370686e7b64c7072ca46",
+    "url": "/portfolio/static/media/expense_tracker_logo.c4a06941.jpg"
+  },
+  {
+    "revision": "e0dff62856ac6ef9f5151c5d32b5a920",
+    "url": "/portfolio/static/media/firebase_logo.e0dff628.jpg"
+  },
+  {
+    "revision": "33771a3aec28192a3495501bf69292c2",
+    "url": "/portfolio/static/media/google_books_logo.33771a3a.jpg"
+  },
+  {
+    "revision": "8d22bc8d4810a3b5d2986fcda27bdd5d",
+    "url": "/portfolio/static/media/hangman_logo.8d22bc8d.jpg"
+  },
+  {
+    "revision": "ea4ed400406c9dbfcedbe204de531dc1",
+    "url": "/portfolio/static/media/javascript_logo.ea4ed400.jpg"
+  },
+  {
+    "revision": "d2b1e19091a3530dfb8bec06b5a926dc",
+    "url": "/portfolio/static/media/material_ui_logo.d2b1e190.png"
+  },
+  {
+    "revision": "8e4d88c4603db273277a08040f8c436c",
+    "url": "/portfolio/static/media/netflix_logo.8e4d88c4.jpg"
+  },
+  {
+    "revision": "6dcf699938b2291d1634f91adb0dded1",
+    "url": "/portfolio/static/media/node_logo.6dcf6999.PNG"
+  },
+  {
+    "revision": "8f7ed6205c84d3aa7f2e9d3aac3f5c74",
+    "url": "/portfolio/static/media/react_logo.8f7ed620.jpg"
+  },
+  {
+    "revision": "4b860080690027111c201d4f6e681ca1",
+    "url": "/portfolio/static/media/redux_logo.4b860080.jpg"
+  },
+  {
+    "revision": "e494939b96f72c6b7b0701b0f1aafd68",
+    "url": "/portfolio/static/media/whatsapp_logo.e494939b.jpg"
+  }
+]);
